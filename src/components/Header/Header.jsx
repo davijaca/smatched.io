1979b4ce69d1aa288c9deda76bad5b87a736d5a1
Davi Bentim <davi.bentim@hotmail.com>
2023-11-18 04:38:02 -0600

    2nd and 3rd sections made

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,27 +24,16 @@ const Header = () => {
         }
     });
 
-    //     const changeColor = () => {
-    //     if (window.scrollY >= 90) {
-    //         setColor(true)
-    //     } else {
-    //         setColor(false)
-    //     }
-    // }
-    //
-    // window.addEventListener('scroll', changeColor)
-
     const closeMenu = () => setClick(false)
 
-    return(
+    return (
         <>
             <div className={styles.mainBox} style={{ background: "transparent" }}>
-
                 <div className={styles.headerBox}>
                     <div className={styles.right}>
 
                         <div className={styles.logo}>
-                            <img src="/img/Horizontal_container.png" alt='logo'/>
+                            <img src="/Horizontal_container.png" alt='logo'/>
                         </div>
                     </div>
 
@@ -79,23 +68,23 @@ const Header = () => {
 
                                 </div>
                             </div>
-
-                            <div>BLOG</div>
+                            <div className={styles.topMenuItem}>BLOG</div>
+                            <div className={styles.howItWorks}>
+                                <button className={styles.btn}>How it works</button>
+                            </div>
                         </div>
 
                     </div>
 
-                    <div className={styles.left}>
-                        <button className={styles.btn}>How it works</button>
 
-                        <div className={styles.hamburger}
-                             onClick={handleClick}
-                        >
-                            {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
-                                : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
+                    <div className={styles.hamburger}
+                        onClick={handleClick}
+                    >
+                        {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
+                            : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
 
-                        </div>
-
+                    </div>
+{/* 
                         <div className={styles.navMenu}>
                             <ul className={click ? "nav-menu active" : "nav-menu"}>
                                 <li className={styles["nav-item"]}>
@@ -139,12 +128,12 @@ const Header = () => {
                                 </li>
                             </ul>
                         </div>
-
+ */}
 
                     </div>
                 </div>
-            </div>
-        </>
+            
+         </>
     )
 }
 
@@ -152,62 +141,3 @@ export default Header;
 
 
 
-//         <div className={color ? 'header header-bg' : 'header'}>
-//                 <div className={styles.headerBox}>
-//                     <div className={styles.right}>
-//                         <div className={styles.rightImg}>
-//                             <img src="./header_logo.png" alt="Logo"/>
-//
-//                         </div>
-//                         <div className={styles.headerTitle}>
-//                             <span>
-//                             <span className={styles.headerTitleItem}>
-//                                 offerwall
-//                             <br />
-//                             </span>
-//                             <span className={styles.headerTitleItem2}>
-//                                 monetization
-//                             </span>
-//                             </span>{" "}
-//                         </div>
-//
-//                         <nav className={styles.topMenu}>
-//                             <a className={styles.topMenuItem} href="/">Home</a>
-//                             {/*<a className={styles.topMenuItem}>How it Works</a>*/}
-//                             <a className={styles.topMenuItem} href="/#/blog">Blog</a>
-//                             <a className={styles.topMenuItem}>About Us</a>
-//                         </nav>
-//                     </div>
-//
-//                     <div className={styles.contact}>
-//                         <a href="/#/contact">
-//                         <div className={styles.contactButton}>Request Contact</div>
-//                         </a>
-//                         <div className={styles.hamburger} onClick={handleClick}>
-//                             {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
-//                                 : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
-//
-//                         </div>
-//                         <ul className={click ? "nav-menu active" : "nav-menu"}>
-//                             <li className={styles["nav-item"]}>
-//                                 <a href='/' onClick={closeMenu}>Home</a>
-//                             </li>
-//                             <li className={styles["nav-item"]}>
-//                                 <a href='/#/blog' onClick={closeMenu}>Blog</a>
-//                             </li>
-//                             <li className={styles["nav-item"]}>
-//                                 <a href='#about' onClick={closeMenu}>About</a>
-//                             </li>
-//                             <li className={styles["nav-item"]}>
-//                                 <a href='/#/contact' onClick={closeMenu}>Contact Us</a>
-//                             </li>
-//                         </ul>
-//                         </div>
-//
-//                 </div>
-//         </div>
-//
-//                 {/* Temporary solution for background opacity */}
-//                 <ul className={click ? "nav-menu active" : "nav-menu"}>
-//                 </ul>
-//
