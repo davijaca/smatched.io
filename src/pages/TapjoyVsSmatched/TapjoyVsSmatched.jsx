dbd9db5a4e954b3c7d39188d0158171f342055b5
francieli.sauthier <francieli.sauthier@gmail.com>
2024-01-02 20:00:29 +0100

    add responsiveness tapjoyVsSmatched first section

--- src/pages/TapjoyVsSmatched/TapjoyVsSmatched.jsx
+++ src/pages/TapjoyVsSmatched/TapjoyVsSmatched.jsx
@@ -12,45 +12,58 @@ const TapjoyVsSmatched = () => {
 
       {/* First TapjoyVsSmatched mobile responsiveness */}
 
-      <div className={styles.mobileFirstTapjoyVsSmatchedContainer}></div>
+      <div className={styles.mobileFirstTapjoyVsSmatchedContainer}>
+        <div className={styles.firstMobileTopImage}>
+          <img src='./images/tapVsSmat_sec1.png' alt='Two smartphones' />
+        </div>
+        <div className={styles.firstMobileBottom}>
+          <h1>Tapjoy vs. Smatched: A Comprehensive Comparison in 2023</h1>
+          <p>
+            Explore the benefits of Tapjoy Offerwall and its comparison with
+            Smatched in 2023. Learn how Tapjoy operates and discover Smatched as
+            a potential alternative. Get insights into their features, pros and
+            cons, pricing, and make an informed choice.
+          </p>
+        </div>
+      </div>
 
-      {/* Second TapjoyVsSmatched */}
+      {/* Second Tapjoy */}
 
       <div className={styles.secondTapjoyVsSmatchedContainer}></div>
 
-      {/* Second TapjoyVsSmatched mobile responsiveness */}
+      {/* Second Tapjoy mobile responsiveness */}
 
       <div className={styles.mobileSecondTapjoyVsSmatchedContainer}></div>
 
-      {/* Third TapjoyVsSmatched */}
+      {/* Third Pros and Cons */}
 
       <div className={styles.thirdTapjoyVsSmatchedContainer}></div>
 
-      {/* Third TapjoyVsSmatched mobile Responsiveness */}
+      {/* Third Pros and Cons mobile Responsiveness */}
 
       <div className={styles.mobileThirdTapjoyVsSmatchedContainer}></div>
 
-      {/* Fourth TapjoyVsSmatched */}
+      {/* Fourth Smatched */}
 
       <div className={styles.fourthTapjoyVsSmatchedContainer}></div>
 
-      {/* Fourth TapjoyVsSmatched mobile Responsiveness */}
+      {/* Fourth Smatched mobile Responsiveness */}
 
       <div className={styles.mobileFourthTapjoyVsSmatchedContainer}></div>
 
-      {/* Fifth TapjoyVsSmatched */}
+      {/* Fifth Offering cards */}
 
       <div className={styles.fifthTapjoyVsSmatchedContainer}></div>
 
-      {/* Fifth TapjoyVsSmatched mobile Responsiveness */}
+      {/* Fifth Offering cards mobile Responsiveness */}
 
       <div className={styles.mobileFifthTapjoyVsSmatchedContainer}></div>
 
-      {/* Sixth TapjoyVsSmatched */}
+      {/* Sixth Conclusion */}
 
       <div className={styles.sixthTapjoyVsSmatchedContainer}></div>
 
-      {/* Sixth TapjoyVsSmatched mobile Responsiveness */}
+      {/* Sixth Conclusion mobile Responsiveness */}
 
       <div className={styles.mobileSixthTapjoyVsSmatchedContainer}></div>
     </div>
