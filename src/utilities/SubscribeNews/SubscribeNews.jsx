bfaf8323a35978b1468693576da70e7815dbce41
knopgm <knop.gm@gmail.com>
2024-04-21 18:56:19 +0200

    Connect useHandleSubscriptionNewsletter to the SubscribeNews component and create space for the message state in the interface of the component

--- src/utilities/SubscribeNews/SubscribeNews.jsx
+++ src/utilities/SubscribeNews/SubscribeNews.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 import styles from './SubscribeNews.module.css';
+import { useHandleSubscriptionNewsletter } from '../../common/useHandleSubscriptionNewsletter';
 
 const SubscribeNews = () => {
+  //Subscription logic extracted from SubscribeNews component and saved on a custom hook at the folder 'common'
+  const { handleInputChange, inputValue, submit, message } =
+    useHandleSubscriptionNewsletter();
+
   return (
     <div className={styles.subscribeNews_wrapper}>
       <div className={styles.subscribeNews}>
@@ -11,17 +16,24 @@ const SubscribeNews = () => {
             Stay up to date
           </h2>
           <h1 className={styles.SubscribeNews_h1}>Join Our Newsletter</h1>
-          <form className={styles.subscribeNews_form}>
+          <form onSubmit={submit} className={styles.subscribeNews_form}>
             <input
               type="email"
               placeholder="Enter your email.."
               className={styles.subscribeNews_input}
+              onChange={handleInputChange}
+              value={inputValue}
             />
             <button type="submit" className={styles.subscribeNews_button}>
               Subscribe
             </button>
           </form>
-          <span className={styles.unsubscribeInfo}>
+        </div>
+        <div className={styles.subscribeInfos_wrapper}>
+          <span className={styles.subscribeInfo}>
+            <p className={styles.subscribe_message}>{message}</p>
+          </span>
+          <span className={styles.subscribeInfo}>
             *You can unsubscribe anytime
           </span>
         </div>
