9a0055c615839539c334fda03bff3e567d03b0a1
knopgm <knop.gm@gmail.com>
2024-05-02 18:52:33 +0200

    Create mobile first style for SubscripeNews Utility box

--- src/utilities/SubscribeNews/SubscribeNews.jsx
+++ src/utilities/SubscribeNews/SubscribeNews.jsx
@@ -16,24 +16,29 @@ const SubscribeNews = () => {
           <h2 className={styles.subscribeNews_stayUpDate_subtitle}>
             Stay up to date
           </h2>
-          <h1 className={styles.SubscribeNews_h1}>Join Our Newsletter</h1>
+          <h1 className={styles.subscribeNews_h1}>Join Our Newsletter</h1>
           <form onSubmit={submit} className={styles.subscribeNews_form}>
             <input
-              type='email'
-              placeholder='Enter your email..'
+              type="email"
+              placeholder="Enter your email.."
               className={styles.subscribeNews_input}
               onChange={handleInputChange}
               value={inputValue}
             />
-            <Button text='SUBSCRIBE' type='submit' />
+            <div className={styles.buttonPlusUnsubscribeInfo_wrapper}>
+              <span className={styles.info_only_mobile}>
+                *You can unsubscribe anytime
+              </span>
+              <Button text="SUBSCRIBE" type="submit" />
+            </div>
           </form>
         </div>
         <div className={styles.subscribeInfos_wrapper}>
           <span className={styles.subscribeInfo}>
             <p className={styles.subscribe_message}>{message}</p>
-          </span>
-          <span className={styles.subscribeInfo}>
-            *You can unsubscribe anytime
+            <span className={styles.info_only_desktop}>
+              *You can unsubscribe anytime
+            </span>
           </span>
         </div>
       </div>
