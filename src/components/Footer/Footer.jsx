af03fc23a660b120badc2b2608e0eaf4f50beace
knopgm <knop.gm@gmail.com>
2024-05-07 16:28:54 +0200

    Organize navbar and footer used images into folders

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = ({ showFormFooter }) => {
           )}
           <div className={styles.bottomLeft}>
             <div className={styles.logo}>
-              <img src='./logo.svg' alt='' />
+              <img src="/images/logo/logo.svg" alt="" />
               <div className={styles.monetize}>
                 <span className={styles.spanMonetize}>
                   Monetize – Unleash your data’s value{' '}
@@ -31,11 +31,11 @@ const Footer = ({ showFormFooter }) => {
             </div>
 
             <div className={styles.social}>
-              <img src='./facebook.svg' alt='' />
-              <img src='./instagram.svg' alt='' />
-              <img src='./linkedin.svg' alt='' />
-              <img src='./tiktok.svg' alt='' />
-              <img src='./youtube.svg' alt='' />
+              <img src="/images/socialMedia/facebook.svg" alt="" />
+              <img src="/images/socialMedia/instagram.svg" alt="" />
+              <img src="/images/socialMedia/linkedin.svg" alt="" />
+              <img src="/images/socialMedia/tiktok.svg" alt="" />
+              <img src="/images/socialMedia/youtube.svg" alt="" />
             </div>
             <div className={styles.copyright}>
               <span className={styles.spanCopy}>
@@ -52,14 +52,14 @@ const Footer = ({ showFormFooter }) => {
                   <div className={styles.subscribeBox}>
                     <input
                       className={styles.inputE}
-                      type='email'
+                      type="email"
                       onChange={handleInputChange}
-                      placeholder='Enter your email..'
+                      placeholder="Enter your email.."
                       value={inputValue}
                     />
                   </div>
                   <div className={styles.submit}>
-                    <Button text='SUBSCRIBE' type='sumit' />
+                    <Button text="SUBSCRIBE" type="sumit" />
                   </div>
                 </form>
               )}
@@ -69,13 +69,13 @@ const Footer = ({ showFormFooter }) => {
           <div className={styles.bottomRight}>
             <div className={styles.supportedBy}>
               <span className={styles.spanTitleBottom}>Supported by </span>
-              <img src='./srh.svg' alt='' />
-              <img src='./innowerft.svg' alt='' />
-              <img src='./startUp.svg' alt='' />
+              <img src="/images/supportedLogos/srh.svg" alt="" />
+              <img src="/images/supportedLogos/innowerft.svg" alt="" />
+              <img src="/images/supportedLogos/startUp.svg" alt="" />
             </div>
             <div className={styles.company}>
               <a className={styles.spanTitleBottom}>Company </a>
-              <a href='/aboutus' className={styles.spanTextBottom}>
+              <a href="/aboutus" className={styles.spanTextBottom}>
                 Our mission{' '}
               </a>
             </div>
@@ -87,13 +87,13 @@ const Footer = ({ showFormFooter }) => {
             </div>
             <div className={styles.legal}>
               <a className={styles.spanTitleBottom}>Legal </a>
-              <a href='/imprint' className={styles.spanTextBottom}>
+              <a href="/imprint" className={styles.spanTextBottom}>
                 Imprint{' '}
               </a>
-              <a href='/dataprivacy' className={styles.spanTextBottom}>
+              <a href="/dataprivacy" className={styles.spanTextBottom}>
                 Data Security{' '}
               </a>
-              <a href='/toc' className={styles.spanTextBottom}>
+              <a href="/toc" className={styles.spanTextBottom}>
                 ToC{' '}
               </a>
               {/* <a className={styles.spanTextBottom}>Cookies</a> */}
@@ -111,14 +111,14 @@ const Footer = ({ showFormFooter }) => {
               <div className={styles.subscribeBoxMobile}>
                 <input
                   className={styles.inputEMobile}
-                  type='email'
+                  type="email"
                   onChange={handleInputChange}
-                  placeholder='Enter your email..'
+                  placeholder="Enter your email.."
                   value={inputValue}
                 />
               </div>
               <div className={styles.submitMobile}>
-                <Button text='SUBSCRIBE' type='sumit' />
+                <Button text="SUBSCRIBE" type="sumit" />
               </div>
             </form>
           </div>
@@ -127,18 +127,18 @@ const Footer = ({ showFormFooter }) => {
 
         <div className={styles.bottomMobile}>
           <div className={styles.topBottomMobile}>
-            <img src='./logo.svg' alt='' />
+            <img src="/images/logo/logo.svg" alt="" />
             <div className={styles.monetizeMobile}>
               <span className={styles.spanMonetizeMobile}>
                 Monetize – Unleash your data’s value{' '}
               </span>
             </div>
             <div className={styles.socialMobile}>
-              <img src='./facebook.svg' alt='' />
-              <img src='./instagram.svg' alt='' />
-              <img src='./linkedin.svg' alt='' />
-              <img src='./tiktok.svg' alt='' />
-              <img src='./youtube.svg' alt='' />
+              <img src="images/socialMedia/facebook.svg" alt="" />
+              <img src="images/socialMedia/instagram.svg" alt="" />
+              <img src="images/socialMedia/linkedin.svg" alt="" />
+              <img src="images/socialMedia/tiktok.svg" alt="" />
+              <img src="images/socialMedia/youtube.svg" alt="" />
             </div>
           </div>
           <div className={styles.bottomDivMobile}>
@@ -146,25 +146,25 @@ const Footer = ({ showFormFooter }) => {
               <span className={styles.spanTitleBottomMobile}>
                 Supported by{' '}
               </span>
-              <img src='./srh.svg' alt='' />
-              <img src='./innowerft.svg' alt='' />
-              <img src='./startUp.svg' alt='' />
+              <img src="/images/supportedLogos/srh.svg" alt="" />
+              <img src="/images/supportedLogos/innowerft.svg" alt="" />
+              <img src="/images/supportedLogos/startUp.svg" alt="" />
             </div>
             <div className={styles.companyMobile}>
               <a className={styles.spanTitleBottomMobile}>Company </a>
-              <a href='/aboutus' className={styles.spanTextBottomMobile}>
+              <a href="/aboutus" className={styles.spanTextBottomMobile}>
                 Our mission{' '}
               </a>
             </div>
             <div className={styles.legalMobile}>
               <a className={styles.spanTitleBottomMobile}>Legal </a>
-              <a href='/imprint' className={styles.spanTextBottomMobile}>
+              <a href="/imprint" className={styles.spanTextBottomMobile}>
                 Imprint{' '}
               </a>
-              <a href='/dataprivacy' className={styles.spanTextBottomMobile}>
+              <a href="/dataprivacy" className={styles.spanTextBottomMobile}>
                 Data Security{' '}
               </a>
-              <a href='/toc' className={styles.spanTextBottomMobile}>
+              <a href="/toc" className={styles.spanTextBottomMobile}>
                 ToC{' '}
               </a>
               {/* <a href='/imprint' className={styles.spanTextBottomMobile}>Cookies</a> */}
